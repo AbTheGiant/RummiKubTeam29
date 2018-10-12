8806c49811af0f7e80d41799c118795652efb8db
henryodai <henryodai@cmail.carleon.ca>
2018-10-12 17:24:54 -0400

    added test case for the deck class with is made of cards

---
+++ src/test/java/core/DeckTest.java
@@ -0,0 +1,42 @@
+package core;
+
+import junit.framework.TestCase;
+
+public class DeckTest extends TestCase {
+
+
+	public void testShuffle()
+    {
+    	try
+    	{
+    	Deck deck1 = new Deck();
+    	Deck deck2 = new Deck();
+
+    	deck2.shuffle();
+
+    	int count = 0;
+
+    	for(int i=0;i<104;i++)
+    	{
+    		Card c1 = deck1.deal();
+    		Card c2 = deck2.deal();
+
+
+    		if( c1.getRank()==c2.getRank() && c1.getColor().equals(c2.getColor())) {
+    			count++;
+    		}
+
+
+    	}
+
+
+    	assertTrue(count < 5);
+    	}
+
+    	catch (Exception e) {
+
+            assertTrue(false);
+		}
+
+    }
+}
