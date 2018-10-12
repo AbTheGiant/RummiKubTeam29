ceb12eebc5a39c32f296882658358e72bc7634c2
Yousef A <mahmoodalmushiri@dhcp-206-181.eduroam.carleton.ca>
2018-10-12 15:40:54 -0400

    added attributes for card class and getters

--- src/main/java/core/Card.java
+++ src/main/java/core/Card.java
@@ -1,6 +1,31 @@
 package core;
 
+import core.Card.Color;
+
 public class Card implements Comparable {
-	
+	public enum Color{Red ,Blue,Green,Orange}
+	private Color color;
+	private int rank;
+	private boolean newCard;
+	private boolean movedCard;
+	public Card(Color color, int rank) {
+		super();
+		this.color = color;
+		this.rank = rank;
+		newCard=false;
+		movedCard=false;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
 	
 }
