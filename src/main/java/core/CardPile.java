a42e4508efc6b86c5238132689c889012cb48f3d
Abiola Olajide <abiolaolajide@dhcp-143-16.cu-wireless-students.carleton.ca>
2018-10-24 16:02:13 -0400

    -added cardpile class methods to make cardPileTest pass completely

---
+++ src/main/java/core/CardPile.java
@@ -0,0 +1,45 @@
+package core;
+
+import java.util.ArrayList;
+
+public class CardPile implements Comparable{
+	protected ArrayList<Card> cards=new ArrayList<Card> ();
+	public  void addCard(Card card)
+	{
+		cards.add(card);
+	}
+	public  int getSize()
+	{
+		return cards.size();
+	}
+	public void addPile(CardPile another)
+	{
+		for(Card card:another.cards)
+		{
+			addCard(card);
+		}
+	}
+	public int getScore()
+	{
+		int result=0;		
+		for(Card card:cards)
+		{
+			result+=card.getRank();
+		}
+		return result;
+		
+	}
+	
+	public int compareTo(Object arg0){
+		CardPile other=(CardPile)arg0;
+		if(this.getScore()>other.getScore())
+			return 1;
+		else if(this.getScore()<other.getScore())
+			return -1;
+		return 0;
+	}
+	public ArrayList<Card> getCards() {
+		return cards;
+	}
+
+}
