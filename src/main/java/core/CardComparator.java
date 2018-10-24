e519c7c411d7336945ed0df1d876918e39f8be32
Yousef A <mahmoodalmushiri@dhcp-201-21.eduroam.carleton.ca>
2018-10-24 17:53:40 -0400

    added CardComprator functions for the test case

---
+++ src/main/java/core/CardComparator.java
@@ -0,0 +1,34 @@
+package core;
+
+import java.util.Comparator;
+
+public class CardComparator implements Comparator<Card> {
+
+	public int compare(Card arg0, Card arg1) {
+		// TODO Auto-generated method stub
+		Card thisCard=(Card)arg0;
+		Card otherCard=(Card)arg1;
+		
+		if(otherCard==null)
+			return 1;
+		if(thisCard.getRank()>otherCard.getRank())
+		{
+			return 1;
+		}
+		else if(thisCard.getRank()<otherCard.getRank())
+		{
+			return -1;
+		}		
+		else if(thisCard.getColorChar()>otherCard.getColorChar())
+		{
+			return 1;
+		}
+		else if(thisCard.getColorChar()<otherCard.getColorChar())
+		{
+			return -1;
+		}
+		else 
+			return 0;
+	}
+
+}
