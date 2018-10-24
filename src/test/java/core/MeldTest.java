4220f4243c2f3939ea6bf0861c3c42411f813822
henryodai <henryodai@cmail.carleon.ca>
2018-10-24 14:16:18 -0400

    meldtest class updated.

--- src/test/java/core/MeldTest.java
+++ src/test/java/core/MeldTest.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import core.Card.Color;
 import junit.framework.TestCase;
 
+
 public class MeldTest extends TestCase {
-	
-	
+
 	public void testSameRankDifferentColour() {
 		int count = 5;
 		try 
@@ -15,68 +15,187 @@ public class MeldTest extends TestCase {
 		Card c2 = new Card (Color.Green,12);
 		Card c3 = new Card (Color.Blue,12);
 		Card c4 = new Card (Color.Orange,12);
+
 		
+
 		if((c1.getRank()== c2.getRank()) &&(c2.getRank()==c3.getRank()) && (c3.getRank() == c1.getRank())) {
+
 			if((c1.getColor() != c2.getColor()) && (c2.getColor() != c3.getColor()) && c3.getColor() != c1.getColor()) {
+
 				count++;
+
 			 }
+
 		}
+
 		assertTrue(count > 5);
+
 		}
+
 		catch(Exception e) {
+
 			assertTrue(false);
+
 		}
+
 		
+
 	}
-	public void testSameColorIncreasing() {
-		try
-		{
-			CardPile p = new CardPile();
-			CardPile newPile = new CardPile();
-		    String firstColor = " ";
-		    int count = 0;
-		    int counter = 0;
-		    firstColor = p.get(0).getColor();
-			for(int j = 0; j<p.getSize(); j++) {
-				if(p.get(j).getColor() == firstColor) {
-					count++;
-				}
-			p.addPile(newPile);	
-			}
-			for(int k = 0; k < p.getSize(); k++) {
-				if(p.get(k) == firstColor) {
-					counter++;
-				}
-			}
-		assertFalse(counter < count);	
-		}
-		catch(Exception e) {
-			assertTrue(false);
-		}
-		
-	}
-	public void testNotSameColor() {
-		try
-		{
-		Card card = new Card(Color.Red,5);
-		Card card1 = new Card(Color.Blue,9);
-		assertEquals(false,card.getColor().equals(card1.getColor()));
-		}
-		catch(Exception e) {
-			assertTrue(false);
-		}
-	}
-	public void testIsValid() {
-		
-	}
-	public void testToString() {
-		
-	}
-	public void testAddMeld() {
-		
-	}
-	public void testGenerateMeld() {
-		
-	}
+
 	
+
+	
+
+	
+
+	public void testSameColorIncreasing() {
+
+		try
+
+		{
+
+			Meld tester = new Meld();
+
+			Card one = new Card (Color.Blue, 10);
+
+			Card two = new Card ( Color.Blue, 11);
+
+			Card three = new Card( Color.Blue, 12);
+
+			tester.addCard(one);
+
+			tester.addCard(two);
+
+			tester.addCard(three);
+
+			assertEquals(true, tester.sameColorIncreasing());
+
+			
+
+			Meld testerTwo = new Meld();
+
+			Card four = new Card (Color.Red, 8);
+
+			Card five = new Card ( Color.Red, 10);
+
+			Card six = new Card( Color.Red, 12);
+
+			testerTwo.addCard(four);
+
+			testerTwo.addCard(five);
+
+			testerTwo.addCard(six);
+
+			assertEquals(false, testerTwo.sameColorIncreasing());
+
+ 			
+
+			
+
+		}
+
+		catch(Exception e) {
+
+			assertTrue(false);
+
+		}
+
+		
+
+	}
+
+	public void testNotSameColor() {
+
+		try
+
+		{
+
+		Card card = new Card(Color.Red,5);
+
+		Card card1 = new Card(Color.Blue,9);
+
+		assertEquals(false,card.getColor().equals(card1.getColor()));
+
+		}
+
+		catch(Exception e) {
+
+			assertTrue(false);
+
+		}
+
+	}
+
+	public void testIsValid() {
+
+		
+
+		Meld tester = new Meld();
+
+		Card one = new Card (Color.Blue, 10);
+
+		Card two = new Card ( Color.Blue, 11);
+
+		Card three = new Card( Color.Blue, 12);
+
+		tester.addCard(one);
+
+		tester.addCard(two);
+
+		tester.addCard(three);
+
+		assertEquals(true, tester.isValid());
+
+		
+
+		Meld testerTwo = new Meld();
+
+		Card four = new Card (Color.Red, 8);
+
+		Card five = new Card ( Color.Red, 10);
+
+		Card six = new Card( Color.Red, 12);
+
+		testerTwo.addCard(four);
+
+		testerTwo.addCard(five);
+
+		testerTwo.addCard(six);
+
+		assertEquals(false, testerTwo.isValid());
+
+		
+
+	}
+
+	public void testToString() {
+
+		Meld tester = new Meld();
+
+		Card one = new Card (Color.Blue, 10);
+
+		Card two = new Card ( Color.Blue, 11);
+
+		Card three = new Card( Color.Blue, 12);
+
+		tester.addCard(one);
+
+		tester.addCard(two);
+
+		tester.addCard(three);
+
+		assertEquals("{B10,B11,B12}", tester.toString());
+
+		
+
+	
+
+		
+
+	}
+
+	
+
+	
+
 }
