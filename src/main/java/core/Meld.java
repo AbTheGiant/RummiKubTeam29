774d5451a376062d97558c22a2c57681b19222ea
phil <philzuby@github>
2018-10-24 18:17:03 -0400

    implementation of meld class

---
+++ src/main/java/core/Meld.java
@@ -0,0 +1,149 @@
+package core;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Meld extends CardPile{
+	//sort cards by	
+	public boolean sameRankDiffentColor()
+	{
+		Collections.sort(cards,new CardComparator());
+		for(int i=0;i<cards.size()-1;i++)
+		{
+			if(cards.get(i).getRank()!=cards.get(i+1).getRank())
+				return false;
+			if(cards.get(i).getColor()==cards.get(i+1).getColor())
+				return false;
+		}		
+		return true;		
+	}
+	
+	public boolean sameColorIncreasing()
+	{
+		Collections.sort(cards);
+		for(int i=0;i<cards.size()-1;i++)
+		{
+			if(cards.get(i).getRank()!=cards.get(i+1).getRank()-1)
+				return false;
+			if(cards.get(i).getColor()!=cards.get(i+1).getColor())
+				return false;
+		}		
+		return true;		
+	}
+	
+	public boolean notSameColor()
+	{
+		for(int i=0;i<cards.size()-1;i++)
+		{
+			if(cards.get(i).getColor()==cards.get(i+1).getColor())
+				return false;
+		}		
+		return true;		
+	}
+	
+	public boolean isValid()
+	{		
+		return (sameColorIncreasing()||sameRankDiffentColor())&&cards.size()>2;		
+	}
+	
+	@Override
+	public String toString() {
+		String result="";
+		for(Card card:cards)
+		{
+			if(result.length()>0)
+			{
+				result+=",";
+			}
+			result+=card.toString();		
+		}
+		return "{"+result+"}";
+	}
+	
+	public void addMeld(Meld anotherMeld)
+	{
+		addPile(anotherMeld);
+	}
+	
+	public ArrayList<Meld> generateMelds() 
+	{	
+		ArrayList<Meld> newMelds=new ArrayList<Meld>(); 
+		Collections.sort(cards);
+		Meld lastMeld=new Meld();
+		for(int i=0;i<cards.size()-1;i++)
+		{
+			if(cards.get(i).getRank()!=cards.get(i+1).getRank()-1
+					||cards.get(i).getColor()!=cards.get(i+1).getColor())
+			{
+				if(cards.get(i).getRank()==cards.get(i+1).getRank()&&
+						cards.get(i).getColor()==cards.get(i+1).getColor())
+				{
+					;					
+				}
+				else
+				{
+					if(lastMeld.getSize()>0)
+					{
+						if(lastMeld.isValid())
+							newMelds.add(lastMeld);
+						lastMeld=new Meld();
+					}
+				}
+			}
+			else
+			{
+				if(lastMeld.getSize()>0)
+					lastMeld.addCard(cards.get(i+1));
+				else
+				{
+					lastMeld.addCard(cards.get(i+1));
+					lastMeld.addCard(cards.get(i));
+				}				
+			}
+		}	
+		// by rank
+		Collections.sort(cards,new CardComparator());
+		for(int i=0;i<cards.size()-1;i++)
+		{
+			
+			if(cards.get(i).getRank()==cards.get(i+1).getRank()
+					&&cards.get(i).getColor()!=cards.get(i+1).getColor())
+			{
+				if(lastMeld.getSize()>0)
+					lastMeld.addCard(cards.get(i+1));
+				else
+				{
+					lastMeld.addCard(cards.get(i+1));
+					lastMeld.addCard(cards.get(i));
+				}
+			}
+			else
+			{
+				if(cards.get(i).getRank()==cards.get(i+1).getRank()&&
+						cards.get(i).getColor()==cards.get(i+1).getColor())
+				{
+					;					
+				}
+				else
+				{
+					if(lastMeld.getSize()>0)
+					{
+						if(lastMeld.isValid())
+							newMelds.add(lastMeld);
+						lastMeld=new Meld();
+					}
+				}
+			}
+		}
+		return newMelds;
+	}
+
+	public int getScore()
+	{
+		if(!isValid())
+			return 0;
+		else
+			return super.getScore();
+	}
+}
--- src/test/java/core/CardComparatorTest.java
+++ src/test/java/core/CardComparatorTest.java
@@ -2,19 +2,54 @@ package core;
 
 import core.Card.Color;
 import junit.framework.TestCase;
-import java.util.Comparator;
+
 
 public class CardComparatorTest extends TestCase{
 	public void testCompare() {
-		Card c1 = new Card(Color.Red,12);
-		Card c2 = new Card(Color.Blue,10);
+	//	Card c1 = new Card(Color.Red,12);
+	//	Card c2 = new Card(Color.Blue,10);
+	//	CardPile p = new CardPile();
+	//	p.addCard(c1);
+	//	p.addCard(c2);
 		
-		assertEquals(1,compare(c1,c2));
+	//	assertEquals(1,compare(p.get(0).getRank(),c2));
 		
-		Card c3 = new Card(Color.Blue,12);
-		Card c4 = new Card(Color.Red,12);
+	//	Card c3 = new Card(Color.Blue,12);
+	//	Card c4 = new Card(Color.Red,12);
 		
-		assertEquals(-1,compare(c3,c4));
+	//	assertEquals(-1,compare(c3,c4));
+		
+		
+		
+		CardPile tester = new CardPile();
+		Card testercard = new Card(Color.Red,11);
+		Card testercard2= new Card(Color.Blue,11);
+		Card testercard3 = new Card(Color.Green,11);
+		tester.cards.add(testercard);
+		tester.cards.add(testercard2);
+		tester.cards.add(testercard3);
+		
+		
+		CardPile tester2 = new CardPile();
+		Card testercard4 = new Card(Color.Orange,11);
+		Card testercard5= new Card(Color.Blue,11);
+		Card testercard6 = new Card(Color.Green,11);
+		tester2.cards.add(testercard4);
+		tester2.cards.add(testercard5);
+		tester2.cards.add(testercard6);
+		
+		
+		CardPile tester3 = new CardPile();
+		Card testercard7 = new Card(Color.Orange,12);
+		Card testercard8= new Card(Color.Blue,11);
+		Card testercard9 = new Card(Color.Green,14);
+		tester3.cards.add(testercard7);
+		tester3.cards.add(testercard8);
+		tester3.cards.add(testercard9);
+		
+		assertTrue(tester.compareTo(tester2)==0);
+		assertTrue(tester.compareTo(tester3)==-1);
+		
 	
 	}
 
