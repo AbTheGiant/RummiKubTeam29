b2e164f095822af0012d38167377bb7e8a7c95b5
Yousef A <mahmoodalmushiri@Yousefs-MacBook.local>
2018-10-29 22:10:27 -0400

    naming fixed

--- src/test/java/core/StrategyTests.java
+++ src/test/java/core/StrategyTests.java
@@ -8,7 +8,7 @@
 	import core.Meld;
 	import junit.framework.TestCase;
 
-public class TestStrategy1 extends TestCase{
+public class TestAIStrategy extends TestCase{
 		
 		@Test
 		public void testStartGameFrom30()
