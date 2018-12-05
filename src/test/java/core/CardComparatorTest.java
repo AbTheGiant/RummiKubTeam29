326e642961aeab4a58e4d9b39d967bf5f51b12d5
Abiola Olajide <abiolaolajide@dhcp-143-62.cu-wireless-students.carleton.ca>
2018-12-05 15:20:04 -0500

    -final changes

--- src/test/java/core/CardComparatorTest.java
+++ src/test/java/core/CardComparatorTest.java
@@ -15,30 +15,30 @@ public class CardComparatorTest extends TestCase{
 		Card testercard = new Card(Color.Red,11);
 		Card testercard2= new Card(Color.Blue,11);
 		Card testercard3 = new Card(Color.Green,11);
-<<<<<<< HEAD
+
 		tester.getCards().add(testercard);
 		tester.getCards().add(testercard2);
 		tester.getCards().add(testercard3);
-=======
+
 		//tester.cards.add(testercard);
 		//tester.cards.add(testercard2);
 		//tester.cards.add(testercard3);
->>>>>>> 4b162a0dc25ec46644bee4ab203ab693f55ec408
+
 		
 		
 		CardPile tester2 = new CardPile();
 		Card testercard4 = new Card(Color.Orange,11);
 		Card testercard5= new Card(Color.Blue,11);
 		Card testercard6 = new Card(Color.Green,11);
-<<<<<<< HEAD
-		tester2.cards.add(testercard4);
+
+		tester2.getCards().add(testercard4);
 		tester2.getCards().add(testercard5);
 		tester2.getCards().add(testercard6);
-=======
+
 		//tester2.cards.add(testercard4);
 		//tester2.cards.add(testercard5);
 		//tester2.cards.add(testercard6);
->>>>>>> 4b162a0dc25ec46644bee4ab203ab693f55ec408
+
 		
 		
 		CardPile tester3 = new CardPile();
